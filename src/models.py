975f9dd5a6fa6066596876c9d370ad26093721d8
pzagmel <pedro.zagalm@gmail.com>
2022-12-22 16:51:57 +0000

    last commit

--- src/models.py
+++ src/models.py
@@ -8,8 +8,8 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
@@ -19,16 +19,34 @@ class Person(Base):
     date_sub = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class FavoriteCharacter(Base):
+    __tablename__ = 'favoritecharacter'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    User_id = Column(Integer, ForeignKey('User.id'))
+    CharacterName = Column(String(250), nullable=False)
+    
+class FavoritePlanet(Base):
+    __tablename__ = 'favoriteplanet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    User_id = Column(Integer, ForeignKey('User.id'))
+    PlanetName = Column(String(250), nullable=False)
+    
+class Characters(Base):    
+    __tablename__ = 'Characters'
+    id = Column(Integer, primary_key=True) 
+    Name = Column(String(50), nullable=False)
+    Age = Column(Integer, nullable=False)
+    Birth_year = Column(Integer, nullable=False)
+    Height = Column(Integer(50), nullable=False)
+    Mass = Column(String(50), nullable=False)
+    Hair_color = Column(String(10), nullable=False)
+    Skin_color = Column(String(10), nullable=False)
+    Eye_color = Column(String(10), nullable=False)
+    Gender = Column(String(10), nullable=False)
 
     def to_dict(self):
         return {}
